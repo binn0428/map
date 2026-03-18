523056114e18524a37eb19e8bbaf656b2a681bb5
binn0428 <156563833+binn0428@users.noreply.github.com>
2026-03-18 21:29:23 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,8 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
-  const repository = process.env.GITHUB_REPOSITORY?.split('/')[1];
-  const owner = process.env.GITHUB_REPOSITORY?.split('/')[0];
-  const isUserOrOrgPagesRepo =
-    repository && owner ? repository.toLowerCase() === `${owner.toLowerCase()}.github.io` : false;
-  const base =
-    process.env.GITHUB_ACTIONS === 'true' && repository && !isUserOrOrgPagesRepo
-      ? `/${repository}/`
-      : '/';
   return {
-    base,
+    base: './',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
